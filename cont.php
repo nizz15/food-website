3068f96e879c26b0441ebc51082575acb8befc8f
Tharushi Nisansala <139553553+nizz15@users.noreply.github.com>
2024-04-15 09:13:27 +0530

    Update cont.php

--- cont.php
+++ cont.php
@@ -7,22 +7,6 @@ $database = "order";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
-/*if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}else{
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
-
-$sql="INSERT INTO contact(name,email,message) value('$name',$email,$message)";
-if (mysqli_query($conn,$sql) === TRUE) {
-  echo "<script>alert('Thank you');window.location.href='cont.php';</script>";
-} else {
-  echo "Error:" . $sql."<br>".mysqli_error($conn);
-}}
-mysqli_close($conn);
-?>*/
-
 if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $email = $_POST['email'];
